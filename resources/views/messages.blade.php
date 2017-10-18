e5c2dd9fff2274cbedd8ac8a2011cf4684ccfafd
zahisajeeb <zahidsajeeb100@gmail.com>
2017-10-18 03:42:55 -0700

    if condition good practise for facing data

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -3,18 +3,23 @@
 @section('content')
 
 
-    @foreach($messages as $message)
-
-        <ul class="list-group">
-            <li class="list-group-item">Name: {{ $message->name }}</li>
-            <li class="list-group-item">Email: {{ $message->email }}</li>
-            <li class="list-group-item">Message: {{ $message->message }}</li>
-
-        </ul>
+    @if(count($messages) > 0)
 
 
-    @endforeach
+        @foreach($messages as $message)
 
+            <ul class="list-group">
+                <li class="list-group-item">Name: {{ $message->name }}</li>
+                <li class="list-group-item">Email: {{ $message->email }}</li>
+                <li class="list-group-item">Message: {{ $message->message }}</li>
+
+            </ul>
+
+
+        @endforeach
+
+
+    @endif
 
 
 @endsection
